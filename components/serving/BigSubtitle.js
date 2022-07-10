e4b1b2ef3f617b8fa7877b726f845f7e888c4669
Emin Pogosbekov <pogosbekovemin@gmail.com>
2022-07-10 15:24:13 +0300

    Big Subtitle created

---
+++ components/serving/BigSubtitle.js
@@ -0,0 +1,7 @@
+export default function BigSubtitle({text}) {
+  return (
+    <h2 className='text-3xl xl:text-5xl font-extrabold text-center leading-10 xl:leading-12 text-zinc-50'>
+      {text}
+    </h2>
+  )
+}
